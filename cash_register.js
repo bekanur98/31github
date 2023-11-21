ac97d35c25642c390210d0e70d360d0e74eef1f6
Beknur Baltabaev <bekanur98@gmail.com>
2023-11-21 17:02:58 +0600

    beautify comments

--- cash_register.js
+++ cash_register.js
@@ -17,16 +17,16 @@ sorted in highest to lowest order, as the value of the change key.
 
 
 ============================================
-|Currency Unit		 |Amount			   |
+|Currency Unit       |Amount               |
 |===========================================
-|Penny				 | $0.01 (PENNY)	   |
-|Nickel				 | $0.05 (NICKEL)	   |
-|Dime				 | $0.1 (DIME)		   |
-|Quarter			 | $0.25 (QUARTER)	   |
-|Dollar				 | $1 (ONE)			   |
-|Five Dollars		 | $5 (FIVE)		   |
-|Ten Dollars		 | $10 (TEN)		   |
-|Twenty Dollars	     | $20 (TWENTY)		   |
+|Penny               | $0.01 (PENNY)       |
+|Nickel              | $0.05 (NICKEL)      |
+|Dime                | $0.1 (DIME)         |
+|Quarter             | $0.25 (QUARTER)     |
+|Dollar              | $1 (ONE)            |
+|Five Dollars        | $5 (FIVE)           |
+|Ten Dollars         | $10 (TEN)           |
+|Twenty Dollars      | $20 (TWENTY)        |
 |One-hundred Dollars | $100 (ONE HUNDRED)  |
 ============================================
 

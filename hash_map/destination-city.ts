21efbe5ffe745ff756a36b30a1c58198cea9d96d
Beknur Baltabaev <bekanur98@gmail.com>
2023-12-15 11:16:27 +0600

    added leetcode problem #1436

---
+++ hash_map/destination-city.ts
@@ -0,0 +1,58 @@
+/*
+
+1436. Destination City
+
+You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+
+It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+ 
+
+Example 1:
+
+Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+Output: "Sao Paulo" 
+Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+Example 2:
+
+Input: paths = [["B","C"],["D","B"],["C","A"]]
+Output: "A"
+Explanation: All possible trips are: 
+"D" -> "B" -> "C" -> "A". 
+"B" -> "C" -> "A". 
+"C" -> "A". 
+"A". 
+Clearly the destination city is "A".
+Example 3:
+
+Input: paths = [["A","Z"]]
+Output: "Z"
+ 
+
+Constraints:
+
+1 <= paths.length <= 100
+paths[i].length == 2
+1 <= cityAi.length, cityBi.length <= 10
+cityAi != cityBi
+All strings consist of lowercase and uppercase English letters and the space character.
+*/
+
+function destCity(paths: string[][]): string {
+    let destMap = paths[0][0];
+    let map = new Map<string, string>();
+    paths.forEach(path => {
+        map.set(path[0], path[1]);
+    });
+
+    paths.forEach(path => {
+        if (!map.has(path[1])) {
+            destMap = path[1];
+        }
+    });
+
+    return destMap;
+};
+
+const result = destCity([["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]);
+console.log(result);
